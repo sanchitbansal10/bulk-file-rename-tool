a1c8ece8b2440a458c7675286b8a3b8ada0d79c1
sanchitbansal10 <sanchitbansal10@gmail.com>
2021-05-30 13:03:57 +0530

    feat(): achieve user story2

--- file_rename.py
+++ file_rename.py
@@ -1,11 +1,35 @@
 import os
 
-file_source = input("Pls provide a file path to be renamed:\n")
-file_destination = input("New file name\n")
-
-new_file_path = os.path.dirname(file_source) + "/" + file_destination
-
-if (os.path.exists(file_source)):
+def change_individual_files(file_source, new_file_name):
+    new_file_path = os.path.dirname(file_source) + "/" + new_file_name
+    print(file_source,new_file_path)
     os.rename(file_source, new_file_path)
+    return "done"
+
+def get_file_names_with_counter(file_name, index):
+    split = file_name.split(".")
+    return split[0] + str(index) + "." + split[1]
+
+def change_folder_file_names(folder_path, new_file_name):
+    files_in_dir = os.listdir(folder_path)
+    for index in range(len(files_in_dir)):
+        full_path = os.path.join(folder_path, files_in_dir[index])
+        if(full_path):
+            change_individual_files(full_path, get_file_names_with_counter(new_file_name, index))
+        else:
+            print("is not a file {}".format(files_in_dir[index]))
+    return "all renamed"
+
+file_source = input("Pls provide a file/folder path to be renamed:\n")
+
+if (not os.path.exists(file_source)):
+    raise Exception('File {} does not exist'.format(file_source))
+
+new_file_name = input("New file name\n")
+
+if (os.path.isdir(file_source)):
+    change_folder_file_names(file_source, new_file_name)
 else:
-    print("error")
+    change_individual_files(file_source, new_file_name)
+    
+
--- idea.md
+++ idea.md
@@ -2,3 +2,7 @@ User Story 1(Achieve renaming single file, nothing fancy here):
     - [X] python script that u can run from command line
     - [X] give it a file path and new file name
     - [X] updates the file name
+
+User Story 2 (Renaming all files in a folder):
+    - [x] python script takes a folder path and new file name
+    - [] rename all the files in that folder path with the new file name and add a counter
